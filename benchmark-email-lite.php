af473c27dfe250005a86f6b12a5a5e5c129f7fd0
beautomated <beautomated@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-05-14 00:05:06 +0000

    tagging version 1.0.1
    git-svn-id: https://plugins.svn.wordpress.org/benchmark-email-lite/trunk@384654 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- benchmark-email-lite.php
+++ benchmark-email-lite.php
@@ -3,7 +3,7 @@
 Plugin Name: Benchmark Email Lite
 Plugin URI: http://www.beautomated.com/benchmark-email-lite/
 Description: A plugin to create a Benchmark Email newsletter widget in WordPress.
-Version: 1.0
+Version: 1.0.1
 Author: beAutomated
 Author URI: http://www.beautomated.com/
 License: GPL
@@ -18,15 +18,16 @@ function benchmarkemaillite_register_widget() {
 	register_widget('benchmarkemaillite_widget');
 }
 
+// Main Class for the Widget
 class benchmarkemaillite_widget extends WP_Widget {
 
-	// Process the new widget
+	// Process the Widget
 	function benchmarkemaillite_widget() {
 		$widget_ops = array(
 			'classname' => 'benchmarkemaillite_widget',
-			'description' => 'Create a Benchmark Email newsletter widget in WordPress.'
+			'description' => __('Create a Benchmark Email newsletter widget in WordPress.')
 		);
-		$this->WP_Widget('benchmarkemaillite_widget', 'Benchmark Email Lite', $widget_ops);
+		$this->WP_Widget('benchmarkemaillite_widget', __('Benchmark Email Lite'), $widget_ops);
 	}
 
 	// Administrative Links
@@ -42,27 +43,39 @@ class benchmarkemaillite_widget extends WP_Widget {
 
 	// Build the Widget Settings Form
 	function form($instance) {
-		$defaults = array('title' => 'Join Our Newsletter', 'page' => '', 'token' => '', 'list' => '');
+		$defaults = array('id' => uniqid(), 'title' => __('Join Our Newsletter'), 'page' => '', 'token' => '', 'list' => '');
 		$instance = wp_parse_args((array) $instance, $defaults);
 ?>
 		<p>
-			Widget Title
+			<?php echo __('Widget Title'); ?>
+			<input name="<?php echo $this->get_field_name('id'); ?>" type="hidden" value="<?php echo uniqid(); ?>" />
 			<input class="widefat" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($instance['title']); ?>" />
 		</p>
 		<p>
-			Limit to Page
-			<?php wp_dropdown_pages("name={$this->get_field_name('page')}&show_option_none=".__('- Show Everywhere -')."&selected=" . esc_attr($instance['page'])); ?>
+			<?php echo __('Limit to Page'); ?>
+<?php
+wp_dropdown_pages(
+	array(
+		'depth' => 0,
+		'child_of' => 0,
+		'selected' => $instance['page'],
+		'echo' => 1,
+		'name' => $this->get_field_name('page'),
+		'show_option_none' => '- ' . __('Show Everywhere') . ' -'
+	)
+);
+?>
 		</p>
 		<p>
-			Your Benchmark Email Token
+			<?php echo __('Your Benchmark Email API Key'); ?>
 			<input class="widefat" name="<?php echo $this->get_field_name('token'); ?>" type="text" value="<?php echo esc_attr($instance['token']); ?>" />
 		</p>
 		<p>
 			<a href="http://www.benchmarkemail.com/?p=68907" target="_blank">
-			Sign-up for a 30-day Free Trial while supporting the plugin Developer!</a>
+			<?php echo __('Signup for a 30-day FREE Trial and support this plugin\'s Development!'); ?></a>
 		</p>
 		<p>
-			Name of Benchmark Email List
+			<?php echo __('Name of Benchmark Email List'); ?>
 			<input class="widefat" name="<?php echo $this->get_field_name('list'); ?>" type="text" value="<?php echo esc_attr($instance['list']); ?>" />
 		</p>
 <?php
@@ -71,58 +84,79 @@ class benchmarkemaillite_widget extends WP_Widget {
 	// Save the Widget Settings
 	function update($new_instance, $old_instance) {
 		$instance = $old_instance;
-		$instance['title'] = strip_tags($new_instance['title']);
-		$instance['page'] = strip_tags($new_instance['page']);
-		$instance['token'] = strip_tags($new_instance['token']);
-		$instance['list'] = strip_tags($new_instance['list']);
+		$instance['id'] = sanitize_text_field($new_instance['id']);
+		$instance['title'] = sanitize_text_field($new_instance['title']);
+		$instance['page'] = absint($new_instance['page']);
+		$instance['token'] = sanitize_text_field($new_instance['token']);
+		$instance['list'] = sanitize_text_field($new_instance['list']);
 		return $instance;
 	}
 
 	// Display the Widget
 	function widget($args, $instance) {
+
+		// Vars
 		global $post;
 		$response = '';
 		extract($args);
+		$key = $instance['id'];
+
+		// Exclude from Pages/Posts
 		if ($instance['page'] && $instance['page'] != $post->ID) { return false; }
+
+		// Begin Outputting Widget
 		echo $before_widget;
 		$title = apply_filters('widget_title', $instance['title']);
 		if (!empty($title)) { echo $before_title . $title . $after_title; }
 		if (empty($instance['token']) || empty($instance['list'])) {
-			echo '<p class="errormsg">Please configure your Benchmark Email Token and List Name.</p>' . $after_widget;
+			echo '<p class="errormsg">' . __('Error: Please configure your Benchmark Email API Key and List Name.') . '</p>' . $after_widget;
 			return false;
 		}
-		if ($_POST['formid'] == 'benchmarkemaillite' && !self::valid_email($_POST['subscribe_email'])) {
-			$response = '<p class="errormsg">Please enter your valid Email Address.</p>';
-		} else if ($email = $_POST['subscribe_email']) {
-			$email = preg_replace('/[^a-zA-Z0-9 @.+_-]/', '', $email);
-			$first = preg_replace('/[^a-zA-Z0-9 \'-]/', '', $_POST['subscribe_first']);
-			$last = preg_replace('/[^a-zA-Z0-9 \'-]/', '', $_POST['subscribe_last']);
-			list($status, $client) = self::connect();
-			if (!$status) { echo $client . $after_widget; return false; }
-			$response = self::subscription($client, $instance['token'], $instance['list'], $email, $first, $last);
+
+		// Process Submission
+		if ($_POST['formid'] == 'benchmark-email-lite-' . $key) {
+
+			// Sanitize Submission
+			$email = sanitize_email($_POST['subscribe_email'][$key]);
+			$first = stripslashes(sanitize_text_field(str_replace('"', '', $_POST['subscribe_first'][$key])));
+			$last = stripslashes(sanitize_text_field(str_replace('"', '', $_POST['subscribe_last'][$key])));
+			if (!self::valid_email($email)) {
+				$response = '<p class="errormsg">' . __('Error: Please enter a valid Email Address.') . '</p>';
+			} else {
+				list($status, $client) = self::connect();
+				if (!$status) { echo $client . $after_widget; return false; }
+				$response = self::subscription($client, $instance['token'], $instance['list'], $email, $first, $last, $key);
+			}
+
+			// Re-process Fields In Case They Were Cleared Upon Success
+			$email = sanitize_email($_POST['subscribe_email'][$key]);
+			$first = stripslashes(sanitize_text_field(str_replace('"', '', $_POST['subscribe_first'][$key])));
+			$last = stripslashes(sanitize_text_field(str_replace('"', '', $_POST['subscribe_last'][$key])));
 		}
+
+		// Finish Outputting Widget
 		echo '
-<form method="post" action="" id="benchmark-email-lite">
+<form method="post" action="#benchmark-email-lite-' . $key . '" id="benchmark-email-lite-' . $key . '">
 <ul style="list-style-type:none;margin:0;">
 	<li>
-		<input type="hidden" name="formid" value="benchmarkemaillite" />
-		<label for="subscribe_first" style="display:block;">First Name</label>
-		<input type="text" id="subscribe_first" name="subscribe_first" value="' . $_POST['subscribe_first'] . '" />
+		<input type="hidden" name="formid" value="benchmark-email-lite-' . $key . '" />
+		<input type="hidden" name="subscribe_key" value="' . $key . '" />
+		<label for="subscribe_first-' . $key . '" style="display:block;">' . __('First Name') . '</label>
+		<input type="text" id="subscribe_first-' . $key . '" name="subscribe_first[' . $key . ']" value="' . $first . '" />
 	</li>
 	<li>
-		<label for="subscribe_last" style="display:block;">Last Name</label>
-		<input type="text" id="subscribe_last" name="subscribe_last" value="' . $_POST['subscribe_last'] . '" />
+		<label for="subscribe_last-' . $key . '" style="display:block;">' . __('Last Name') . '</label>
+		<input type="text" id="subscribe_last-' . $key . '" name="subscribe_last[' . $key . ']" value="' . $last . '" />
 	</li>
 	<li>
-		<label for="subscribe_email" style="display:block;">Email Address (required)</label>
-		<input type="text" id="subscribe_email" name="subscribe_email" value="' . $_POST['subscribe_email'] . '" />
+		<label for="subscribe_email-' . $key . '" style="display:block;">' . __('Email Address (required)') . '</label>
+		<input type="text" id="subscribe_email-' . $key . '" name="subscribe_email[' . $key . ']" value="' . $email . '" />
 	</li>
-	<li><input id="subscribe_submit" name="subscribe_submit" type="submit" value="Subscribe" /></li>
+	<li><input type="submit" value="' . __('Subscribe') . '" /></li>
 	<li>' . $response . '</li>
 </ul>
 </form>
-';
-		echo $after_widget;
+' . $after_widget;
 	}
 
 	// Validate Email Address
@@ -135,7 +169,7 @@ class benchmarkemaillite_widget extends WP_Widget {
 	function connect() {
 		require_once(ABSPATH . WPINC . '/class-IXR.php');
 		if (!class_exists(IXR_Client)) {
-			return array(false, '<p class="errormsg">IXR Client library isn\'t installed.</p>');
+			return array(false, '<p class="errormsg">' . __('Error: IXR Client library isn\'t installed.') . '</p>');
 		}
 		return array(true, new IXR_Client('http://api.benchmarkemail.com/1.0/'));
 	}
@@ -143,7 +177,7 @@ class benchmarkemaillite_widget extends WP_Widget {
 	// Get Existing Subscriber Data
 	function lookup($client, $token, $listID, $email) {
 		$client->query('listGetContacts', $token, $listID, $email, 1, 100, 'name', 'asc');
-		//$client->query('listGetContactDetails', $token, $listID, $email); THIS API CALL IS BROKEN!
+		//$client->query('listGetContactDetails', $token, $listID, $email); THIS BME API CALL IS BROKEN!
 		if ($client->isError()) {
 			return array(false, '<p class="errormsg">Error: [C] ' . $client->getErrorMessage()) . '</p>';
 		}
@@ -152,7 +186,7 @@ class benchmarkemaillite_widget extends WP_Widget {
 	}
 
 	// Add or Update Subscriber
-	function subscription($client, $token, $list, $email, $first, $last) {
+	function subscription($client, $token, $list, $email, $first, $last, $key) {
 
 		// Locate List to Subscribe Onto
 		$client->query('listGet', $token, '', 1, 100, 'name', 'asc');
@@ -164,7 +198,7 @@ class benchmarkemaillite_widget extends WP_Widget {
 		foreach ($lists as $listdata) {
 			if (strtolower($listdata['listname']) == strtolower($list)) { $listID = $listdata['id']; }
 		}
-		if (!$listID) { return '<p class="errormsg">Error: Unable to locate the specified list.</p>'; }
+		if (!$listID) { return '<p class="errormsg">' . __('Error: Unable to locate the specified list.') . '</p>'; }
 
 		// Check for Pre-Existance
 		list($status, $contactID) = self::lookup($client, $token, $listID, $email);
@@ -190,7 +224,10 @@ class benchmarkemaillite_widget extends WP_Widget {
 				)
 			);
 			if ($client->isError()) { return 'Error: [U] ' . $client->getErrorMessage(); }
-			return '<p class="successmsg">Successful. Thank you.</p>';
+			$_POST['subscribe_first'][$key] = '';
+			$_POST['subscribe_last'][$key] = '';
+			$_POST['subscribe_email'][$key] = '';
+			return '<p class="successmsg">' . __('Successful. Thank you.') . '</p>';
 		}
 	}
 }
--- readme.txt
+++ readme.txt
@@ -2,15 +2,15 @@
 Contributors: beautomated
 Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=B22PPZ3SC6WZE
 Tags: widget, widgets, api, list, email, mail, news, register, registration, plugin, plugins, wordpress, sidebar, newsletter
-Requires at least: 2.8.6
+Requires at least: 2.9
 Tested up to: 3.1.2
-Stable tag: 1.0
+Stable tag: 1.0.1
 
 Benchmark Email Lite creates a newsletter signup form widget.
 
 == Description ==
 
-Benchmark Email Lite creates a newsletter signup form widget. The widget creates a simple W3C and WAVE validated signup form to instantly subscribe visitors to a Benchmark Email contact list, requesting the subscriber's first name, last name, and email address. If the subscriber pre-exists on the list, this will update the subscriber's first and last names on the mailing list.
+Benchmark Email Lite creates a newsletter signup form widget. The widget creates a simple W3C and WAVE validated signup form to instantly subscribe visitors to a Benchmark Email contact list, requesting the subscriber's first name, last name, and email address. If the subscriber preexists on the list, this will update the subscriber's first and last names on the mailing list.
 
 The signup form uses standard HTML list items so it can be manipulated by CSS within your theme to match your site's design. Some themes will automatically style the widget to match the design. Contact us or the designer of your theme if you are having difficulties getting the form to match your theme, or add your own child theme with CSS that styles the elements accordingly.
 
@@ -35,13 +35,13 @@ Please [Contact Us](http://www.beautomated.com/contact/ "Contact Us") if you hav
 1. If you are creating a new Benchmark Email account, please use the link [http://www.benchmarkemail.com/?p=68907](http://www.benchmarkemail.com/?p=68907 "Try Benchmark Email") to support the development of this plugin.
 1. Obtain your Benchmark Email API Key by logging into Benchmark Email, click My Account, click My Account Settings, scroll to the big yellow box towards the bottom of the page and copy the API Key code.
 1. Expand the Widget options. Enter an optional title, your Benchmark Email API Key and the name of the list for your visitors to be subscribed to. There is an optional setting to limit the plugin to a single page, if desired. Click Save when you're all done.
-1. Test the plugin by going to your site and completing the form with temporary dummy data. You should see a "Successful" message. Verify that the subscription went through by logging into Benchmark Email, clicking List, choosing your list. The new subscription should be at the top. You can delete the dummy entry once verified.
+1. Test the plugin by going to your site and completing the form with temporary dummy data. You should see a "Successful. Thank you." message. Verify that the subscription went through by logging into Benchmark Email, clicking List, choosing your list. The new subscription should be at the top. You can delete the dummy entry once verified.
 
 == Frequently Asked Questions ==
 
 = How do I make the form match my theme? =
 
-The signup form uses standard HTML list items so it can be manipulated by CSS within you theme to match your site's design. Some themes will automatically style the widget to match the design. Contact the designer of your theme if you are having difficulties getting the form to match your theme, or add your own child theme with CSS that styles the elements accordingly.
+The signup form uses standard HTML list items so it can be manipulated by CSS within you theme to match your site's design. Some themes will automatically style the widget to match the design. Contact the designer of your theme if you are having difficulties getting the form to match your theme, or add your own child theme with CSS that styles the elements accordingly. The main classname value is 'benchmarkemaillite_widget'.
 
 = How do I control which page(s) the widget appears on, or subscribe to multiple lists? =
 
@@ -52,8 +52,34 @@ There is an optional setting to limit the plugin to a single page, if desired. T
 * [Widgets on Pages](http://wordpress.org/extend/plugins/widgets-on-pages/ "Get Widgets on Pages Plugin")
 * [Add Widgets to Pages](http://wordpress.org/extend/plugins/add-widgets-to-pages/ "Get Add Widgets to Pages Plugin")
 
+= I use Kuberick theme and I'm seeing bullets! =
+
+We have confirmed that the Kuberick theme adds bullets in the sidebar. We cannot override this without forcing a new stylesheet, which would have a negative effect on other themes. So, we recommend you override this within your CSS with the entry at the bottom '.entry ul li::before,#sidebar ul ul li::before{content:"";}'
+
 == Screenshots ==
 
 1. This is the widget control panel.
 2. This is the default widget on Twenty Ten theme.
 3. This is an example of the widget with customized CSS.
+
+== Changelog ==
+
+= 1.0.1 =
+
+* Fixed bug in first name, last name, and email address submitted data sanitizing to be compatible with international symbols or anything that WordPress considers safe for data validation purposes. Reference: 'sanitize_email()' and 'sanitize_text_field()' functions on WordPress Codex.
+* Fixed bug when the widget is installed multiple times on a single page leading to only one form pre-populating the entered data and some CSS conflicts. Multiple instances per page are now supported!
+* Updated admin area widget field sanitization method to 'sanitize_text_field()' function requiring v2.9.0.
+* Added anchor '#benchmark-email-lite' into URL so that after form submission it puts the user on the proper screen position to view the server response.
+* Adjusted the server response to clear out the submitted values upon successful form submission.
+* Re-titled the Benchmark Email Token to the term "API Key" to match what Benchmark Email is calling it on their website.
+* Added support for international language translation/localization.
+
+= 1.0 =
+
+* Initial release.
+
+== Upgrade Notice ==
+
+= 1.0 =
+
+* Fixes a couple of bugs and adds a few minor features!
