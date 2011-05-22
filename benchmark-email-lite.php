daba78f456d6e170c0266e80815d5f36ea30dc53
beautomated <beautomated@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-05-22 21:24:52 +0000

    Uploading development version 1.0.2.3 unstable.
    git-svn-id: https://plugins.svn.wordpress.org/benchmark-email-lite/trunk@387769 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- benchmark-email-lite.php
+++ benchmark-email-lite.php
@@ -3,7 +3,7 @@
 Plugin Name: Benchmark Email Lite
 Plugin URI: http://www.beautomated.com/benchmark-email-lite/
 Description: A plugin to create a Benchmark Email newsletter widget in WordPress.
-Version: 1.0.2.2
+Version: 1.0.2.3
 Author: beAutomated
 Author URI: http://www.beautomated.com/
 License: GPL
@@ -140,6 +140,10 @@ class benchmarkemaillite_widget extends WP_Widget {
 		// Exclude from Pages/Posts Per Setting
 		if ($instance['page'] && $instance['page'] != $post->ID) { return false; }
 
+		// Output Widget Title, If Exists
+		$title = apply_filters('widget_title', $instance['title']);
+		if (!empty($title)) { echo $before_title . $title . $after_title; }
+
 		// Display Any Submission Response
 		if (is_array(self::$response) && array_key_exists($key, self::$response) && is_array(self::$response[$key])) {
 			$printresponse = (self::$response[$key][0])
@@ -158,10 +162,6 @@ class benchmarkemaillite_widget extends WP_Widget {
 				return true;
 			}
 		}
-
-		// Output Widget Title, If Exists
-		$title = apply_filters('widget_title', $instance['title']);
-		if (!empty($title)) { echo $before_title . $title . $after_title; }
 
 		// Output Setup Notification, If Widget Not Yet Setup
 		if (empty($instance['token']) || empty($instance['list'])) {
