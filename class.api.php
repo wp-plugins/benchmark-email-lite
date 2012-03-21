020814cc3b98dccd6802f8e4a42efc9910de281a
beautomated <beautomated@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-21 05:28:47 +0000

    Added new API timeout throttling logic in version 2.3 to version 2.2. Ready for testing.
    git-svn-id: https://plugins.svn.wordpress.org/benchmark-email-lite/tags/2.2@521688 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- class.api.php
+++ class.api.php
@@ -1,48 +1,68 @@
 <?php
 
 class benchmarkemaillite_api {
-	static $client, $token, $listid, $connected, $campaignid;
+	static $token, $listid, $campaignid, $timeout = 5;
 
-	// Attempt To Connect To Benchmark Email
-	function connect() {
-		if (self::$connected) { return; }
+	// Executes Query with Time Tracking
+	function query() {
+		ini_set('default_socket_timeout', self::$timeout);
 		require_once(ABSPATH . WPINC . '/class-IXR.php');
-		self::$client = new IXR_Client(benchmarkemaillite::$apiurl);
-		self::$connected = true;
+
+		// Skip This Request If Temporarily Disabled
+		if ($disabled = get_transient('benchmark-email-lite_serverdown')) { return; }
+
+		// Connect and Communicate
+		$client = new IXR_Client(benchmarkemaillite::$apiurl, false, 443, self::$timeout);
+		$time = time();
+		$args = func_get_args();
+		call_user_func_array(array($client, 'query'), $args);
+		$response = $client->getResponse();
+
+		// If Over Limit, Disable for Five Minutes And Produce Warning
+		$time = (time() - $time);
+		if ($time >= self::$timeout) {
+			set_transient('benchmark-email-lite_serverdown', true, 300);
+			set_transient(
+				'benchmark-email-lite_errors',
+				__('Error connecting to Benchmark Email API server. Connection throttled until', 'benchmark-email-lite')
+				. ' ' . date('H:i:s', (current_time('timestamp') + 300))
+				. ' ' . __('to prevent sluggish behavior.', 'benchmark-email-lite'),
+				300
+			);
+			return false;
+		}
+		return $response;
 	}
 
 	// Register Vendor
 	function handshake($tokens) {
 		if (!$tokens || !is_array($tokens)) { return; }
-		self::connect();
 		foreach ($tokens as $token) {
 			if (!$token) { continue; }
-			self::$client->query('UpdatePartner', $token, 'beautomated');
+			return self::query('UpdatePartner', $token, 'beautomated');
 		}
 	}
 
 	// Lookup Lists For Account
 	function lists() {
-		self::connect();
-		self::$client->query('listGet', self::$token, '', 1, 100, 'name', 'asc');
-		$response = self::$client->getResponse();
-		return isset($response['faultCode']) ? $response['faultCode'] : $response;
+		if ($response = self::query('listGet', self::$token, '', 1, 100, 'name', 'asc')) {
+			return isset($response['faultCode']) ? $response['faultCode'] : $response;
+		}
+		return false;
 	}
 
 	// Get Existing Subscriber Data
 	function find($email) {
-		self::connect();
-		self::$client->query(
-			'listGetContacts', self::$token, self::$listid, $email, 1, 100, 'name', 'asc'
-		);
-		if (self::$client->isError()) {
-			return array(false, self::$client->getErrorMessage());
+		if ($response = self::query(
+				'listGetContacts', self::$token, self::$listid, $email, 1, 100, 'name', 'asc'
+			)
+		) {
+			return (
+				is_array($response) && array_key_exists(0, $response)
+				&& is_array($response[0]) && array_key_exists('id', $response[0])
+			) ? $response[0]['id'] : false;
 		}
-		$data = self::$client->getResponse();
-		return (
-			is_array($data) && array_key_exists(0, $data)
-			&& is_array($data[0]) && array_key_exists('id', $data[0])
-		) ? $data[0]['id'] : false;
+		return false;
 	}
 
 	// Add or Update Subscriber
@@ -53,21 +73,18 @@ class benchmarkemaillite_api {
 		$data['email'] = $data['Email'];
 		$contactID = self::find($data['Email']);
 		if (!is_numeric($contactID) && $contactID != false) { return $contactID; }
-		self::connect();
 
 		// Doesn't Pre-Exist, Add New Subscription
 		if (!is_numeric($contactID)) {
-			self::$client->query('listAddContactsOptin', self::$token, self::$listid, array($data), '1');
-			if (self::$client->isError()) {
-				return array(false, self::$client->getErrorMessage());
+			if (!self::query('listAddContactsOptin', self::$token, self::$listid, array($data), '1')) {
+				return array(false, '');
 			}
 			return array(true, __('A verification email has been sent.', 'benchmark-email-lite'));
 		}
 
 		// Or Update Preexisting Subscription
-		self::$client->query('listUpdateContactDetails', self::$token, self::$listid, $contactID, $data);
-		if (self::$client->isError()) {
-			return array(false, 'Error: [Updt] ' . self::$client->getErrorMessage());
+		if (!self::query('listUpdateContactDetails', self::$token, self::$listid, $contactID, $data)) {
+			return array(false, '');
 		}
 		return array(true, __('Successfully updated subscription.', 'benchmark-email-lite'));
 	}
@@ -83,12 +100,11 @@ class benchmarkemaillite_api {
 			'webpageVersion' => $webpageVersion,
 			'permissionReminderMessage' => $permissionMessage,
 		);
-		self::connect();
 
 		// Check For Preexistance
-		self::$client->query('emailGet', self::$token, $title, '', 1, 1, '', '');
-		$response = self::$client->getResponse();
-		self::$campaignid = isset($response[0]['id']) ? $response[0]['id'] : false;
+		if ($response = self::query('emailGet', self::$token, $title, '', 1, 1, '', '')) {
+			self::$campaignid = isset($response[0]['id']) ? $response[0]['id'] : false;
+		}
 
 		// Handle Preexisting And Sent Campaign
 		if (self::$campaignid && $response[0]['status'] == 'Sent') {
@@ -99,39 +115,34 @@ class benchmarkemaillite_api {
 		// Update Existing Campaign
 		if (self::$campaignid) {
 			$data['id'] = self::$campaignid;
-			self::$client->query('emailUpdate', self::$token, $data);
-			$response = self::$client->getResponse();
-			return self::$client->isError() ? false : __('updated', 'benchmark-email-lite');
+			if ($response = self::query('emailUpdate', self::$token, $data)) {
+				return (!$response) ? false : __('updated', 'benchmark-email-lite');
+			}
 		}
 
 		// Create New Campaign
-		self::$client->query('emailCreate', self::$token, $data);
-		self::$campaignid = self::$client->getResponse();
-		return self::$client->isError() ? self::$client->getErrorMessage() : __('created', 'benchmark-email-lite');
+		if ($response = self::query('emailCreate', self::$token, $data)) {
+			return (!$response) ? false : __('created', 'benchmark-email-lite');
+		}
+		return false;
 	}
 
 	// Test Email Campaign
 	function campaign_test($to) {
 		if (!is_numeric(self::$campaignid) || !$to) { return; }
-		self::connect();
-		self::$client->query('emailSendTest', self::$token, self::$campaignid, $to);
-		return self::$client->isError();
+		return self::query('emailSendTest', self::$token, self::$campaignid, $to);
 	}
 
 	// Send Email Campaign
 	function campaign_now() {
 		if (!is_numeric(self::$campaignid)) { return; }
-		self::connect();
-		self::$client->query('emailSendNow', self::$token, self::$campaignid);
-		return self::$client->isError();
+		return self::query('emailSendNow', self::$token, self::$campaignid);
 	}
 
 	// Schedule Email Campaign
 	function campaign_later($when) {
 		if (!is_numeric(self::$campaignid)) { return; }
-		self::connect();
-		self::$client->query('emailSchedule', self::$token, self::$campaignid, $when);
-		return self::$client->isError();
+		return self::query('emailSchedule', self::$token, self::$campaignid, $when);
 	}
 }
 
