c049ac3327f6493792ec04ac3b7b736ca9a09624
beautomated <beautomated@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-08-15 16:19:33 +0000

    Tagging version 2.4 for production.
    git-svn-id: https://plugins.svn.wordpress.org/benchmark-email-lite/tags/2.4@585881 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ class.api.php
@@ -0,0 +1,165 @@
+<?php
+
+class benchmarkemaillite_api {
+	static $token, $listid, $campaignid;
+
+	// Executes Query with Time Tracking
+	function query() {
+		$options = get_option('benchmark-email-lite_group');
+		$timeout = (isset($options[5]) && $options[5] > 10) ? $options[5] : 10;
+		ini_set('default_socket_timeout', $timeout);
+		require_once(ABSPATH . WPINC . '/class-IXR.php');
+
+		// Skip This Request If Temporarily Disabled
+		if ($disabled = get_transient('benchmark-email-lite_serverdown')) { return; }
+
+		// Connect and Communicate
+		$client = new IXR_Client(benchmarkemaillite::$apiurl, false, 443, $timeout);
+		$time = time();
+		$args = func_get_args();
+		call_user_func_array(array($client, 'query'), $args);
+		$response = $client->getResponse();
+
+		// If Over Limit, Disable for Five Minutes And Produce Warning
+		$time = (time() - $time);
+		if ($time >= $timeout) {
+			set_transient('benchmark-email-lite_serverdown', true, 300);
+			set_transient(
+				'benchmark-email-lite_errors',
+				__('Error connecting to Benchmark Email API server. Connection throttled until', 'benchmark-email-lite')
+				. ' ' . date('H:i:s', (current_time('timestamp') + 300))
+				. ' ' . __('to prevent sluggish behavior.', 'benchmark-email-lite')
+				. ' ' . __('If this occurs frequently, try increasing your' , 'benchmark-email-lite')
+				. ' <a href="options-general.php?page=benchmark-email-lite&amp;tab=settings">'
+				. __('Connection Timeout setting.', 'benchmark-email-lite') . '</a>',
+				300
+			);
+			return false;
+		}
+		return $response;
+	}
+
+	// Register Vendor
+	function handshake($tokens) {
+		if (!$tokens || !is_array($tokens)) { return; }
+		foreach ($tokens as $token) {
+			if (!$token) { continue; }
+			return self::query('UpdatePartner', $token, 'beautomated');
+		}
+	}
+
+	// Lookup Lists For Account
+	function lists() {
+		if ($response = self::query('listGet', self::$token, '', 1, 100, 'name', 'asc')) {
+			return isset($response['faultCode']) ? $response['faultCode'] : $response;
+		}
+		return false;
+	}
+
+	// Get Existing Subscriber Data
+	function find($email) {
+		if ($response = self::query(
+				'listGetContacts', self::$token, self::$listid, $email, 1, 100, 'name', 'asc'
+			)
+		) {
+			return (
+				is_array($response) && array_key_exists(0, $response)
+				&& is_array($response[0]) && array_key_exists('id', $response[0])
+			) ? $response[0]['id'] : false;
+		}
+		return false;
+	}
+
+	// Add or Update Subscriber
+	function subscribe($data) {
+
+		// Check for Subscription Preexistance
+		if (!isset($data['Email'])) { return array(false, '[No Email Address]'); }
+		$data['email'] = $data['Email'];
+		$contactID = self::find($data['Email']);
+		if (!is_numeric($contactID) && $contactID != false) { return $contactID; }
+
+		// Doesn't Pre-Exist, Add New Subscription
+		if (!is_numeric($contactID)) {
+			if (!self::query('listAddContactsOptin', self::$token, self::$listid, array($data), '1')) {
+				return array(false, '');
+			}
+			return array(true, __('A verification email has been sent.', 'benchmark-email-lite'));
+		}
+
+		// Or Update Preexisting Subscription
+		if (!self::query('listUpdateContactDetails', self::$token, self::$listid, $contactID, $data)) {
+			return array(false, '');
+		}
+		return array(true, __('Successfully updated subscription.', 'benchmark-email-lite'));
+	}
+
+	// Create Email Campaign
+	function campaign($title, $from, $subject, $body, $webpageVersion, $permissionMessage) {
+		$data = array(
+			'emailName' => $title,
+			'toListID' => (int)self::$listid,
+			'fromName' => $from,
+			'subject' => $subject,
+			'templateContent' => $body,
+			'webpageVersion' => $webpageVersion,
+			'permissionReminderMessage' => $permissionMessage,
+		);
+
+		// Check For Preexistance
+		if ($response = self::query('emailGet', self::$token, $title, '', 1, 1, '', '')) {
+			self::$campaignid = isset($response[0]['id']) ? $response[0]['id'] : false;
+		}
+
+		// Handle Preexisting And Sent Campaign
+		if (self::$campaignid && $response[0]['status'] == 'Sent') {
+			self::$campaignid = false;
+			return __('preexists', 'benchmark-email-lite');
+		}
+
+		// Update Existing Campaign
+		if (self::$campaignid) {
+			$data['id'] = self::$campaignid;
+			if ($response = self::query('emailUpdate', self::$token, $data)) {
+				return (!$response) ? false : __('updated', 'benchmark-email-lite');
+			}
+		}
+
+		// Create New Campaign
+		if ($response = self::query('emailCreate', self::$token, $data)) {
+			self::$campaignid = $response;
+			return (!$response) ? false : __('created', 'benchmark-email-lite');
+		}
+		return false;
+	}
+
+	// Test Email Campaign
+	function campaign_test($to) {
+		if (!is_numeric(self::$campaignid) || !$to) { return; }
+		return self::query('emailSendTest', self::$token, self::$campaignid, $to);
+	}
+
+	// Send Email Campaign
+	function campaign_now() {
+		if (!is_numeric(self::$campaignid)) { return; }
+		return self::query('emailSendNow', self::$token, self::$campaignid);
+	}
+
+	// Schedule Email Campaign
+	function campaign_later($when) {
+		if (!is_numeric(self::$campaignid)) { return; }
+		return self::query('emailSchedule', self::$token, self::$campaignid, $when);
+	}
+
+	// Get Email Campaigns
+	function campaigns() {
+		return self::query('reportGet', self::$token, '', 1, 25, 'date', 'desc');
+	}
+
+	// Get Email Campaign Report Summary
+	function campaign_summary($id) {
+		return self::query('reportGetSummary', self::$token, $id);
+	}
+}
+
+?>
---
+++ reports.detail.html.php
@@ -0,0 +1,134 @@
+<div style="float:left;width:500px;">
+	<h3><?php echo __('Email Summary', 'benchmark-email-lite'); ?></h3>
+	<p>
+		<strong><?php echo __('Email name', 'benchmark-email-lite'); ?>:</strong> <?php echo $response['emailName']; ?>
+		<br /><strong><?php echo __('Subject', 'benchmark-email-lite'); ?>:</strong> <?php echo $response['subject']; ?>
+	</p>
+	<div style="height:400px;">
+		<div id="chart_div"></div>
+	</div>
+	<h3><?php echo __('Email Reports', 'benchmark-email-lite'); ?></h3>
+	<p>
+		<form method="post" action="">
+		<input type="submit" class="button-primary" name="show" value="<?php echo __('Opens', 'benchmark-email-lite'); ?>"
+			title="<?php echo __('Click to view report', 'benchmark-email-lite'); ?>"
+			<?php if (!$response['opens']) { echo ' disabled="disabled"';} ?> />
+		<input type="submit" class="button-primary" name="show" value="<?php echo __('Clicks', 'benchmark-email-lite'); ?>"
+			title="<?php echo __('Click to view report', 'benchmark-email-lite'); ?>"
+			<?php if (!$response['clicks']) { echo ' disabled="disabled"';} ?> />
+		<input type="submit" class="button-primary" name="show" value="<?php echo __('Bounces', 'benchmark-email-lite'); ?>"
+			title="<?php echo __('Click to view report', 'benchmark-email-lite'); ?>"
+			<?php if (!$response['bounces']) { echo ' disabled="disabled"';} ?> />
+		<input type="submit" class="button-primary" name="show" value="<?php echo __('Unsubscribes', 'benchmark-email-lite'); ?>"
+			title="<?php echo __('Click to view report', 'benchmark-email-lite'); ?>"
+			<?php if (!$response['unsubscribes']) { echo ' disabled="disabled"';} ?> />
+		<input type="submit" class="button-primary" name="show" value="<?php echo __('Forwards', 'benchmark-email-lite'); ?>"
+			title="<?php echo __('Click to view report', 'benchmark-email-lite'); ?>"
+			<?php if (!$response['forwards']) { echo ' disabled="disabled"';} ?> />
+		<input type="submit" class="button-primary" name="show" value="<?php echo __('Unopens', 'benchmark-email-lite'); ?>"
+			title="<?php echo __('Click to view report', 'benchmark-email-lite'); ?>"
+			<?php if (!$response['unopens']) { echo ' disabled="disabled"';} ?> />
+		</form>
+	</p>
+</div>
+<div style="float:left;">
+	<h3><?php echo __('Email Statistics', 'benchmark-email-lite'); ?></h3>
+	<table class="widefat" cellspacing="0">
+		<thead>
+			<tr>
+				<th><?php echo __('Statistic', 'benchmark-email-lite'); ?></th>
+				<th><?php echo __('Value', 'benchmark-email-lite'); ?></th>
+				<th><?php echo __('Percent', 'benchmark-email-lite'); ?></th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td><?php echo __('Total Emails Sent', 'benchmark-email-lite'); ?></td>
+				<td><?php echo $response['mailSent']; ?></td>
+				<td> </td>
+			</tr>
+			<tr>
+				<td><?php echo __('Opened Emails', 'benchmark-email-lite'); ?></td>
+				<td>
+					<?php echo ($response['opens']) ? "<a href='{$url}opens' title='" . __('Click to view report', 'benchmark-email-lite') . "'>{$response['opens']}</a>" : 0; ?>
+				</td>
+				<td>
+					<?php echo number_format(100*$response['opens']/$response['mailSent'], 1); ?>%
+				</td>
+			</tr>
+			<tr>
+				<td><?php echo __('Links Clicked', 'benchmark-email-lite'); ?></td>
+				<td>
+					<?php echo ($response['clicks']) ? "<a href='{$url}clicks' title='" . __('Click to view report', 'benchmark-email-lite') . "'>{$response['clicks']}</a>" : 0; ?>
+				</td>
+				<td>
+					<?php echo number_format(100*$response['clicks']/$response['opens'], 1); ?>%
+				</td>
+			</tr>
+			<tr>
+				<td><?php echo __('Emails Forwarded', 'benchmark-email-lite'); ?></td>
+				<td>
+					<?php echo ($response['forwards']) ? "<a href='{$url}forwards' title='" . __('Click to view report', 'benchmark-email-lite') . "'>{$response['forwards']}</a>" : 0; ?>
+				</td>
+				<td> </td>
+			</tr>
+			<tr>
+				<td><?php echo __('Emails Bounced', 'benchmark-email-lite'); ?></td>
+				<td>
+					<?php echo ($response['bounces']) ? "<a href='{$url}bounces' title='" . __('Click to view report', 'benchmark-email-lite') . "'>{$response['bounces']}</a>" : 0; ?>
+				</td>
+				<td>
+					<?php echo number_format(100*$response['bounces']/$response['mailSent'], 1); ?>%
+				</td>
+			</tr>
+			<tr>
+				<td><?php echo __('Unsubscribes', 'benchmark-email-lite'); ?></td>
+				<td>
+					<?php echo ($response['unsubscribes']) ? "<a href='{$url}unsubscribes' title='" . __('Click to view report', 'benchmark-email-lite') . "'>{$response['unsubscribes']}</a>" : 0; ?>
+				</td>
+				<td>
+					<?php echo number_format(100*$response['unsubscribes']/$response['mailSent'], 1); ?>%
+				</td>
+			</tr>
+			<tr>
+				<td><?php echo __('Unopened', 'benchmark-email-lite'); ?></td>
+				<td>
+					<?php echo ($response['unopens']) ? "<a href='{$url}unopens' title='" . __('Click to view report', 'benchmark-email-lite') . "'>{$response['unopens']}</a>" : 0; ?>
+				</td>
+				<td>
+					<?php echo number_format(100*$response['unopens']/($response['mailSent']), 1); ?>%
+				</td>
+			</tr>
+			<tr>
+				<td><?php echo __('Abuse Reports', 'benchmark-email-lite'); ?></td>
+				<td><?php echo $response['abuseReports']; ?></td>
+				<td> </td>
+			</tr>
+		</tbody>
+	</table>
+	<?php self::showLocations(); ?>
+</div>
+<div style="clear:both;"> </div>
+<script type="text/javascript" src="https://www.google.com/jsapi"></script>
+<script type="text/javascript">
+google.load("visualization", "1", {packages:["corechart"]});
+google.setOnLoadCallback(drawChart);
+function drawChart() {
+	var data = google.visualization.arrayToDataTable([
+		['Item', 'Quantity'],
+		['<?php echo __('Opened', 'benchmark-email-lite'); ?>', <?php echo $response['opens'];?>],
+		['<?php echo __('Bounced', 'benchmark-email-lite'); ?>', <?php echo $response['bounces'];?>],
+		['<?php echo __('Unopened', 'benchmark-email-lite'); ?>', <?php echo $response['unopens'];?>]
+	]);
+	var options = {
+		chartArea: { width: 400, height: 365 },
+		width: 500,
+		height: 400,
+		is3D: true,
+		legend: { position: 'bottom' },
+		colors: ['77D9A1', 'F2A81D', '1C8DDE']
+	};
+	var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+	chart.draw(data, options);
+}
+</script>
